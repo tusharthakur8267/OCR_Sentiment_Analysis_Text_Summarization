b532441534993024498ae642f00d14fe0f04c992
Tushar Thakur <98010596+tusharthakur8267@users.noreply.github.com>
2025-03-18 11:35:29 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -74,71 +74,7 @@ def index():
 
         
 
-        # summarization : 1
-#         stopwords = list(STOP_WORDS)
-
-#         nlp = spacy.load('en_core_web_sm')
-
-#         doc = nlp(text2)
-#         tokens = [token.text for token in doc]
-#         # print(tokens)
-#         punctuation = ""
-#         punctuation = punctuation + '\n'
-#         # punctuation
-
-#         word_frequencies = {}
-#         for word in doc:
-#             if word.text.lower() not in stopwords:
-#                 if word.text.lower() not in punctuation:
-#                     if word.text not in word_frequencies.keys():
-#                         word_frequencies[word.text] = 1
-#                     else:
-#                         word_frequencies[word.text] += 1
-
-#         # print(word_frequencies)
-
-#         max_frequency = max(word_frequencies.values())
-#         # max_frequency
-
-#         for word in word_frequencies.keys():
-#                 word_frequencies[word] = word_frequencies[word]/max_frequency
-
-#         # print(word_frequencies)
-
-#         sentence_tokens = [sent for sent in doc.sents]
-#         # print(sentence_tokens)
-
-#         sentence_scores = {}
-
-#         for sent in sentence_tokens:
-#             for word in sent:
-#                 if word.text.lower() in word_frequencies.keys():
-#                     if sent not in sentence_scores.keys():
-#                         sentence_scores[sent] = word_frequencies[word.text.lower()]
-#                     else:
-#                         sentence_scores[sent] += word_frequencies[word.text.lower()]
-
-#         # sentence_scores
-#         select_length = int(len(sentence_tokens)*0.3)
-#         # select_length
-#         summary = nlargest(select_length, sentence_scores,key=sentence_scores.get)
-#         # summary
-#         final_summary = [word.text for word in summary]
-#         summary = ' '.join(final_summary)
-#         t1=" ".join(summary.split())
-#         # TreebankWordDetokenizer().detokenize(['the', 'quick', 'brown'])
-#         t3 =TreebankWordDetokenizer().detokenize(final_summary)
-#         samm = t1
-
-#         # print(type(t1))
-#         print(t1)
-#         print(t3)
-#         print(summary)
-#         print(text2)
-#         # print("hello my name is tushar")
-#         # print(text2)
-# #
-        # summarization : 2
+        # summarization : 
         stopWords = set(stopwords.words("english"))
         words = word_tokenize(text)
 
